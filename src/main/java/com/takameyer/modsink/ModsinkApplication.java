e7227826fb2de11be4d9939d7a03651c7e787022
Andrew Meyer <andrew.meyer@mongodb.com>
2024-11-26 09:42:03 +0100

    Init from Springintializr

---
+++ src/main/java/com/takameyer/modsink/ModsinkApplication.java
@@ -0,0 +1,13 @@
+package com.takameyer.modsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takameyer/modsink/ModsinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takameyer.modsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
